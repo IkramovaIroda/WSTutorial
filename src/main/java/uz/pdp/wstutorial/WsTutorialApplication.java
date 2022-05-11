c86f3527450ff2303ce9499bed2b3db8e0876cb8
JafarbekTurayev <jafarbek1997@gmail.com>
2022-05-11 11:26:04 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/wstutorial/WsTutorialApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.wstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/wstutorial/WsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.wstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
